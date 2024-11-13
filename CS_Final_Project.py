98cb4559a3e340b3815a1cfea6f913cb13298094
areekiemell <areekiemell@umass.edu>
2024-11-13 16:04:07 -0600

    Update CS_Final_Project.py

--- CS_Final_Project.py
+++ CS_Final_Project.py
@@ -16,6 +16,24 @@ def print_board(player_board,enemy_board):
 def game_rules():
     print("Hi! Welcome to Battleship. \n Let's explain the rules before we start the game. You will place 5 ships: one is 5 units long, one is 4 units long, two are 3 units long, and one is 2 units long. \n Once you place them, you'll pick coordinates on your opponent's grid to try to sink all of their ships. If they sink all of yours, you lose.\n There are some power-ups to be found, but we'll explain those if you find them. Good luck and have fun!")
 
+class Battleship:
+    def __init__(self, name, length, orientation, start_position):
+        """name: name of the ship
+           length: length of the ship
+           orientation: orientation of the ship
+           start_position: starting position of the ship on the grid"""
+        self.name = name
+        self.length = length
+        self.orientation = orientation
+        self.start_position = start_position
+
+        self.positions = calculate_positions():
+    def calculate_positions(self):
+        positions = []
+        row, col = self.start_position
+
+
+
 def create_enemy_ships(enemy_board):
     # create 5 ships for the user to place
 
